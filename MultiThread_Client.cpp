d4135e4b9befc806155cb77bc470ef4fb9666a2e
KwonSangMin <38064756+KwonSangMin@users.noreply.github.com>
2021-04-14 22:34:43 +0900

    Update MultiThread_Client.cpp

--- MultiThread_Client.cpp
+++ MultiThread_Client.cpp
@@ -6,6 +6,7 @@
 #include <sys/wait.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <string>
 #define BUF_SIZE 1024
 #define PORT 2802
 #define SERVERADDR "localhost"
@@ -18,7 +19,7 @@ int main()
         client_fd=socket(PF_INET,SOCK_STREAM,0);
         if(client_fd==-1)
         {
-          printf("SOCKET ERROR\n");
+                printf("SOCKET ERROR\n");
         }
         memset(&server_addr,0,sizeof(server_addr));
         server_addr.sin_family=PF_INET;
@@ -31,23 +32,31 @@ int main()
         memset(buffer,0,1024);
         printf("WAITING RECV FROM SERVER\n");
         /*int recv_length=0;
-        while(recv_length<30)
+        while(recv_length<30)//클라이언트와 서버가 연결될 경우 서버의 자식 프로세스가 클라이언트에게 메세지를 보낸다. 하지만 네트워크 상황에따라 언제 패킷이 도착할지 알수없다. 또한, 클라이언트에서는 서버가 얼마만큼의 데이터를 보내는지
+ 알 수 없기 때문에 기존에 데이터양을 합의하거나, 전송 데이터 크기를 미리 전달해주어야한다. 그이후에 원하는 데이터 크>기만큼 수신이 될 때까지 무한루프를 돌려서 계속해서 패킷을 읽어오도록 해야한다.
+  {
+    recv_length+=recv(client_fd,buffer,30,0);
+  }
+  printf("Client :: Message From Server -> %s\n",buffer);*/
+	while(1){
+	scanf("%s", buffer);
+         if(buffer[0]=='q'||buffer[0]=='Q')
         {
-          recv_length+=recv(client_fd,buffer,30,0);
+                printf("Close the Connection With %c\n",buffer[0]);
+                break;
         }
-        printf("Client :: Message From Server -> %s\n",buffer);*/
-
-        scanf("%s", buffer);
-        int buffersize= sizeof(buffer);
-        printf("buffersize=%d\n",buffersize);
+	int buffersize= strlen(buffer);
+	printf("buffersize=%d\n",buffersize);
         send(client_fd,(char*)&buffersize,4,0);
-        send(client_fd,buffer,1024,0);
-        int recv_length=0;
-        while(recv_length<30)
-        {
-          memset(&buffer,0,1024);
-          recv_length+=recv(client_fd,buffer,30,0);
-        }
-        printf("Send Finished\n");
+        send(client_fd,buffer,buffersize,0);
+	int recv_length=0;
+	memset(&buffer,0,1024);
+	while(recv_length<1024)
+	{
+		recv_length+=recv(client_fd,buffer,1024,0);
+	}
+	printf("Send Finished\n");
+	}
         close(client_fd);
 }
+
